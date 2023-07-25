ecd78cd94c2888b31af06453092ab1b683f93a3c
rafsantos <rlsantos@gmail.com>
2023-07-24 21:39:04 -0300

    Add files via upload

--- ml.py
+++ ml.py
@@ -2,7 +2,10 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import pickle
 import pandas
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing
 
 
 df = pandas.read_csv('IRIS.csv')
@@ -11,4 +14,5 @@ model = KNeighborsClassifier(n_neighbors=3)
 features = list(zip(df["sepal_length"], df["sepal_width"]))
 
 model.fit(features,df["species"])
+
 pickle.dump(model, open('model.pkl','wb'))
